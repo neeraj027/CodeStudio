afd4870d91205556b1d20a0d160333bcb9c6f076
neeraj027 <neerajkshatriy@gmail.com>
2023-11-17 11:58:07 +0530

    N-Forest.py added

---
+++ N-Forest.py
@@ -0,0 +1,9 @@
+def nForest(n:int) ->None:
+    # Write your solution here.
+    for i in range(n):
+        for i in range(n):
+            print ('*',end=" ")
+        print()
+        
+            
+    pass
